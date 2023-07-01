7c0e2c4480ee7d4ef3b4f57a3b9e31d004229fd0
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-07-01 22:40:15 +0200

    dropdowns are staying when mouseover and mthod for getting currencys

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', main);
 
 let currencyNames = [];
+let currencys = [];
 
 async function main() {
   currencyNames = await getAllCurrencyNames();
+  currencys = await getAllCurrencys();
   addEventListeners();
 }
 
@@ -13,24 +15,33 @@ function addEventListeners() {
   const dropdownButtonFrom = document.querySelector('.dropdown-button-from');
   const dropdownButtonTo = document.querySelector('.dropdown-button-to');
 
+  dropdownButtonFrom.addEventListener('mouseover', renderDropdownElementsFromButton);
   dropdownButtonFrom.addEventListener('mouseover', () => {
     dropdownChoicesFrom.classList.add('show-dropdown');
   });
-  dropdownButtonFrom.addEventListener('mouseover', renderDropdownElements);
-
   dropdownButtonFrom.addEventListener('mouseleave', () => {
     dropdownChoicesFrom.classList.remove('show-dropdown');
   });
+  dropdownChoicesFrom.addEventListener('mouseover', () => {
+    dropdownChoicesFrom.classList.add('show-dropdown');
+  });
+  dropdownChoicesFrom.addEventListener('mouseleave', () => {
+    dropdownChoicesFrom.classList.remove('show-dropdown');
+  });
 
-
+  dropdownButtonTo.addEventListener('mouseover', renderDropdownElementsToButton);
   dropdownButtonTo.addEventListener('mouseover', () => {
     dropdownChoicesTo.classList.add('show-dropdown');
   });
-
   dropdownButtonTo.addEventListener('mouseleave', () => {
     dropdownChoicesTo.classList.remove('show-dropdown');
   });
-
+  dropdownChoicesTo.addEventListener('mouseover', () => {
+    dropdownChoicesTo.classList.add('show-dropdown');
+  });
+  dropdownChoicesTo.addEventListener('mouseleave', () => {
+    dropdownChoicesTo.classList.remove('show-dropdown');
+  });
 }
 
 async function getAllCurrencyNames() {
@@ -51,7 +62,36 @@ async function getAllCurrencyNames() {
 
 }
 
-function renderDropdownElements() {
+async function getAllCurrencys() {
+  const apiUrl = "https://v6.exchangerate-api.com/v6/ddf6ee9624da92869f6b9028/latest/sek";
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    const currencyRates = data.conversion_rates;
+    const currencys = [];
+
+    for (const currencyCode in currencyRates) {
+      if (currencyRates.hasOwnProperty(currencyCode)) {
+        const currency = {
+          code: currencyCode,
+          rate: currencyRates[currencyCode]
+        };
+        currencys.push(currency);
+      }
+    }
+    return currencys;
+
+  } catch (error) {
+    console.log(error);
+    debugger;
+  }
+
+}
+
+
+function renderDropdownElementsFromButton() {
   const dropdownDiv = document.querySelector("#dropdown-choices-from");
   currencyNames.forEach(currencyName => {
     const optionElement = document.createElement("a");
@@ -61,5 +101,22 @@ function renderDropdownElements() {
     dropdownDiv.appendChild(optionElement);
 
   });
+
+}
+
+function renderDropdownElementsToButton() {
+  const dropdownDiv = document.querySelector("#dropdown-choices-to");
+  currencyNames.forEach(currencyName => {
+    const optionElement = document.createElement("a");
+    optionElement.value = currencyName;
+    optionElement.text = currencyName;
+
+    dropdownDiv.appendChild(optionElement);
+
+  });
+
+}
+
+function getConvertedCurrency(currencyName, value){
 
 }
