b78bbfbda9375065dca3e9613c3c6346b84fb78b
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-07-12 16:24:34 +0200

    init functions

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', main);
 import config from './config.js';
 import { initCurrencyExchanger } from './currencyExchanger.js';
 import { addEventListenersForCurrencyExchanger } from './currencyExchanger.js';
-import { renderChart } from './chartRendering.js';
+import { initChartRendering } from './chartRendering.js';
 
 
 async function main() {
 
   await initCurrencyExchanger();
   await addEventListenersForCurrencyExchanger();
-  await renderChart();
+  await initChartRendering();
 }
 
 
