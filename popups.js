f1cd41f2942314c528cbc82bbbac6b485111d522
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-07-15 17:20:23 +0200

    nu är det annan text på utropstecknet men den ska fixas och ta in rätt värde.n.

--- popups.js
+++ popups.js
@@ -1,4 +1,5 @@
-let isPopupOpen = false;
+let isStrongestPopupOpen = false;
+let isWeakestPopupOpen = false;
 let isFeedbackOpen = false;
 
 export async function initPopupRender() {
@@ -6,24 +7,38 @@ export async function initPopupRender() {
 }
 
 async function addEventListenerForPopups() {
-  const exclamationMarkIcons = document.querySelectorAll('.fa-circle-exclamation');
-
   const feedbackBtn = document.querySelector("#feedback-btn");
   feedbackBtn.addEventListener("click", await toggleFeedbackPopup);
 
-  for (let i = 0; i < exclamationMarkIcons.length; i++) {
-    const icon = exclamationMarkIcons[i];
-    icon.addEventListener('click', async (event) => {
-      const clickX = event.clientX;
-      const clickY = event.clientY;
-      await toggleExclamationPopup(clickX, clickY);
-    });
+  const strongestIcon = document.querySelector('#strongest-icon');
+  const weakestIcon = document.querySelector('#weakest-icon');
 
-  }
-  const closePopupIcons = document.querySelector('.popup-close-icon');
-  closePopupIcons.addEventListener('click', closeExclamationPopup);
+  // for (let i = 0; i < exclamationMarkIcons.length; i++) {
+  //   const icon = exclamationMarkIcons[i];
+  //   icon.addEventListener('click', async (event) => {
+  //     const clickX = event.clientX;
+  //     const clickY = event.clientY;
+  //     await toggleExclamationPopup(clickX, clickY);
+  //   });
 
-  await addEventListenerForSendingFeedback();
+  strongestIcon.addEventListener('click', async (event) => {
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+    await toggleExclamationPopup(clickX, clickY, "strongest");
+  });
+
+  weakestIcon.addEventListener('click', async (event) => {
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+    await toggleExclamationPopup(clickX, clickY, "weakest");
+  });
+
+  //vi får se om detta funkar: ? men ej säkert att dom kan heta samma icons såhär utan foreach
+  // const popUpDiv = document.querySelector('.exclamation-popup-div');
+  // const closePopupIcons = document.querySelector('.popup-close-icon');
+  // closePopupIcons.addEventListener('click', closePopup(popUpDiv));
+
+ 
 }
 
 // async function saveFeedback() {
@@ -114,69 +129,95 @@ async function renderFeedbackPopup() {
   document.body.appendChild(divElement);
 }
 
-async function renderExclamationMarkPopup(clickX, clickY) {
-  const divElement = document.createElement('div');
-  divElement.classList.add('exclamation-popup-div');
+async function renderExclamationMarkPopup(clickX, clickY, icon) {
+  if (icon == "weakest") {
 
-  if (window.innerWidth <= 768) {
-    // Mobilstorlek - ändra positionen
-    divElement.style.left = clickX + 120 + 'px';
-    divElement.style.top = clickY + 50 + 'px';
-  } else {
-    // Annars använd tidigare positionering
-    divElement.style.left = clickX + 200 + 'px';
-    divElement.style.top = clickY + 70 + 'px';
+    const divElement = document.createElement('div');
+    divElement.classList.add('exclamation-weakest-popup-div');
+
+    // if (window.innerWidth <= 768) {
+    //   // Mobilstorlek - ändra positionen
+    //   divElement.style.left = clickX + 120 + 'px';
+    //   divElement.style.top = clickY + 50 + 'px';
+    // } else {
+    //   // Annars använd tidigare positionering
+    //   divElement.style.left = clickX + 200 + 'px';
+    //   divElement.style.top = clickY + 70 + 'px';
+    // }
+    const popupHeader = document.createElement('div');
+    popupHeader.classList.add('popup-header');
+
+    const titleElement = document.createElement('h3');
+    titleElement.classList.add('popup-date');
+    titleElement.textContent = "Information" ?? "Laddar datum...";
+
+    const text = document.createElement('h4');
+    text.classList.add('popup-title');
+    text.innerHTML = "När du växlar från {basvalutan} får du mindre av {den svagaste valutan}.";
+    popupHeader.appendChild(titleElement);
+    // popupHeader.appendChild(icon);
+
+    // Här sätter jag de olika elementen till själva popupen
+    divElement.appendChild(popupHeader);
+    divElement.appendChild(text);
+
+    // Och här sätts hela popupen som barn till DOMen
+    document.body.appendChild(divElement);
   }
+  else if (icon == "strongest") {
+    const divElement = document.createElement('div');
+    divElement.classList.add('exclamation-strongest-popup-div');
 
-  const popupHeader = document.createElement('div');
-  popupHeader.classList.add('popup-header');
+    const popupHeader = document.createElement('div');
+    popupHeader.classList.add('popup-header');
 
-  const titleElement = document.createElement('h3');
-  titleElement.classList.add('popup-date');
-  titleElement.textContent = "Information" ?? "Laddar datum...";
+    const titleElement = document.createElement('h3');
+    titleElement.classList.add('popup-date');
+    titleElement.textContent = "Information" ?? "Laddar datum...";
 
-  //LÄGGER TILL DENA IGEN MED FUNKTION NÄR TID FINNS
-  // const icon = document.createElement('i');
-  // icon.classList.add('fa', 'fa-window-close', 'popup-close-icon'); // Lägger till klassen 'popup-close-icon'
-  // icon.setAttribute('aria-hidden', 'true');
+    const text = document.createElement('h4');
+    text.classList.add('popup-title');
+    text.innerHTML = "När du växlar från basvalutan får du mer av den starkaste valutan.";
+    popupHeader.appendChild(titleElement);
+    // popupHeader.appendChild(icon);
 
+    // Här sätter jag de olika elementen till själva popupen
+    divElement.appendChild(popupHeader);
+    divElement.appendChild(text);
 
-  //text för svagaste:
-  //När du växlar från {basvalutan} får du mindre av {den svagaste valutan}.
-
-  //text
-  //När du växlar från basvalutan får du mer av den starkaste valutan.
-
-
-  const text = document.createElement('h4');
-  text.classList.add('popup-title');
-  text.innerHTML = "För tillfället hanterar vi inte alla valutor, för att se vilka som är med i beräkningen<br>vänligen se listan för knapparna ovan" ?? "Laddar text...";
-  popupHeader.appendChild(titleElement);
-  // popupHeader.appendChild(icon);
-
-  // Här sätter jag de olika elementen till själva popupen
-  divElement.appendChild(popupHeader);
-  divElement.appendChild(text);
-
-  // Och här sätts hela popupen som barn till DOMen
-  document.body.appendChild(divElement);
-}
-
-async function toggleExclamationPopup(clickX, clickY) {
-
-  if (isPopupOpen) {
-    const popUpDiv = document.querySelector('.exclamation-popup-div');
-    popUpDiv.remove(); // Stäng popupen om den är öppen
-    isPopupOpen = false;
-  } else {
-    await renderExclamationMarkPopup(clickX, clickY);
-    isPopupOpen = true;
-  }
-}
-
-function closeExclamationPopup() {
-  const popUpDiv = document.querySelector('.exclamation-popup-div');
-  if (popUpDiv !== null) {
-    popUpDiv.remove();
+    // Och här sätts hela popupen som barn till DOMen
+    document.body.appendChild(divElement);
   }
 }
+
+async function toggleExclamationPopup(clickX, clickY, icon) {
+
+  if (icon == "strongest") {
+    if (isStrongestPopupOpen) {
+      const popUpDiv = document.querySelector('.exclamation-strongest-popup-div');
+      popUpDiv.remove(); // Stäng popupen om den är öppen
+      isStrongestPopupOpen = false;
+    } else {
+      await renderExclamationMarkPopup(clickX, clickY, "strongest");
+      isStrongestPopupOpen = true;
+    }
+  }
+  else if (icon == "weakest") {
+    if (isWeakestPopupOpen) {
+      const popUpDiv = document.querySelector('.exclamation-weakest-popup-div');
+      popUpDiv.remove(); // Stäng popupen om den är öppen
+      isWeakestPopupOpen = false;
+    } else {
+      await renderExclamationMarkPopup(clickX, clickY, "weakest");
+      isWeakestPopupOpen = true;
+    }
+  }
+
+}
+
+
+// function closePopup(popupDiv) {
+//   if (popUpDiv !== null) {
+//     popUpDiv.remove();
+//   }
+// }
