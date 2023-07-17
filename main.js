ff782b4e704cf90e54883ef266023e0d62e89df7
Angelina Holmqvist <angelinaholmqvist@live.se>
2023-07-17 23:17:45 +0200

    news can write out to console

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', main);
-import config from './config.js';
 import { initCurrencyExchanger } from './currencyExchanger.js';
 import { addEventListenersForCurrencyExchanger } from './currencyExchanger.js';
 import { initChartRendering } from './chartRendering.js';
 import { initPopupRender } from './popups.js';
+import { initNews } from './news.js';
 
 
 async function main() {
@@ -11,6 +11,7 @@ async function main() {
   await addEventListenersForCurrencyExchanger();
   await initChartRendering();
   await initPopupRender();
+  await initNews();
 }
 
 
